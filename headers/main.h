e7f3b97076cb0332801bcd699bca4fcf81b34358
BArs303 <bayanov_2003@mail.ru>
2025-01-26 16:11:33 +0300

    Empty window creation (GLFW library and GLAD)

--- headers/main.h
+++ headers/main.h
@@ -1,1 +1,4 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 #include <stdio.h>
--- source/main.c
+++ source/main.c
@@ -2,6 +2,32 @@
 
 int main()
 {
-	printf("Hello world!\n");
+	GLFWwindow *window;
+	if(!glfwInit())
+		return -1;
+	window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
+	if(!window)
+	{
+		printf("window creation failed\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+	{
+		printf("GLAD load failed\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
